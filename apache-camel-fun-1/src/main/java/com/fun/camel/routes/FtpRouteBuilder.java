7478aa2672902cfe82be3ac65e5758d10c448668
Roger Viñas <roger.vinas@scmspain.com>
2018-02-16 01:15:04 +0100

    Fix constructor

--- apache-camel-fun-1/src/main/java/com/fun/camel/routes/FtpRouteBuilder.java
+++ apache-camel-fun-1/src/main/java/com/fun/camel/routes/FtpRouteBuilder.java
@@ -1,10 +1,24 @@
 package com.fun.camel.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Component
 public class FtpRouteBuilder extends RouteBuilder {
+
+    private final Duration retryDelay;
+    private final int retryCount;
+
+    public FtpRouteBuilder(
+            @Value("ftp.retry.delay") String retryDelay,
+            @Value("ftp.retry.count") int retryCount
+    ) {
+        this.retryDelay = Duration.parse(retryDelay);
+        this.retryCount = retryCount;
+    }
 
     @Override
     public void configure() {
